45c86799897e601048299fe1630bf7ac95b44e96
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-04-08 19:44:21 +0200

    ask_removal_style def

--- src/askers.py
+++ src/askers.py
@@ -4,8 +4,36 @@ from tkinter import filedialog
 
 
 
-def ask_removal_style() -> Literal[""]:
-    return ""
+def ask_removal_style() -> Literal[
+        "normal_deleting",
+        "normal_deleting_recursive",
+        "cautious_deleting",
+        "cautious_deleting_recursive",
+        "exit"]:
+    returns_dict = {
+        "n":  "normal_deleting",
+        "nr": "normal_deleting_recursive",
+        "c":  "cautious_deleting",
+        "cr": "cautious_deleting_recursive",
+        "e":  "exit"}
+
+    while True:
+        print("Normal deleting means deleting all DS_Store files and ones starting with '._'\n"
+              "Cautious deleting means deleting all DS_Store files and only AppleDouble files \n"
+              "that have a corresponding file in current directory.\n\n"
+              "Choose deleting option:\n"
+              "n  - Normal deleting\n"
+              "nr - Normal deleting (recursive)\n"
+              "c  - Cautious deleting\n"
+              "cr - Cautious deleting (recursive)\n"
+              "e  - Exit\n"
+              ">> ", end="")
+        asker = input().strip().lower()
+
+        if asker in returns_dict:
+            return returns_dict[asker]
+        else:
+            print("Invalid input\n\n")
 
 
 def ask_path_filedialog() -> Path | None:
